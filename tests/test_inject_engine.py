624781063b84576c3f86449d5a8739d1d75c913a
Lee Kelvin <lkelvin@astro.princeton.edu>
2026-06-10 08:44:29 -0700

    Update unit tests to check infer gain function

--- tests/test_inject_engine.py
+++ tests/test_inject_engine.py
@@ -19,6 +19,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import logging
 import unittest
 from types import GeneratorType
 
@@ -30,11 +31,12 @@ import lsst.utils.tests
 from lsst.geom import Point2D, SpherePoint, degrees
 from lsst.source.injection.inject_engine import (
     generate_galsim_objects,
+    infer_gain_from_image,
     inject_galsim_objects_into_exposure,
     make_galsim_object,
 )
 from lsst.source.injection.utils.test_utils import make_test_exposure, make_test_injection_catalog
-from lsst.utils.tests import MemoryTestCase, TestCase
+from lsst.utils.tests import TestCase
 
 
 class InjectEngineTestCase(TestCase):
@@ -88,6 +90,23 @@ class InjectEngineTestCase(TestCase):
             self.assertIsInstance(galsim_object[2], int)  # draw size
             self.assertIsInstance(galsim_object[3], GSObject)  # GSObject
 
+    def test_infer_gain_nonexistent_mask(self):
+        """Test that infer_gain_from_image returns a gain value and logs a
+        warning when provided with nonexistent mask plane names.
+        """
+        logger = logging.getLogger(__name__)
+        with self.assertLogs(logger, level="WARNING") as cm:
+            gain = infer_gain_from_image(
+                self.exposure,
+                bad_mask_names=["NONEXISTENT_MASK1", "NONEXISTENT_MASK2"],
+                logger=logger,
+            )
+        self.assertTrue(any("NONEXISTENT_MASK1" in msg for msg in cm.output))
+        self.assertIsInstance(gain, float)
+        self.assertTrue(np.isfinite(gain))
+        gain_no_mask = infer_gain_from_image(self.exposure, bad_mask_names=[])
+        self.assertAlmostEqual(gain, gain_no_mask)
+
     def test_inject_galsim_objects_into_exposure(self):
         flux0 = np.sum(self.exposure.image.array)
         injected_outputs = inject_galsim_objects_into_exposure(
@@ -113,7 +132,7 @@ class InjectEngineTestCase(TestCase):
         self.assertTrue(all(isinstance(item, bool) for item in injected_outputs[3]))  # PSF compute errors
 
 
-class MemoryTestCase(MemoryTestCase):
+class MemoryTestCase(lsst.utils.tests.MemoryTestCase):
     """Test memory usage of functions in this script."""
 
     pass
